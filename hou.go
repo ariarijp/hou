c5b62ae68a454f18ac5382159e68c987c33b9706
ariarijp <takuya.arita@gmail.com>
2017-09-25 22:15:29 +0900

    Add -quiet and -silent options Improve error handling Print input text by default

--- hou.go
+++ hou.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -34,35 +35,62 @@ func createParams(filename string) slack.PostMessageParameters {
 	return params
 }
 
+func handleError(err error, quiet bool) {
+	if !quiet {
+		log.Fatal(err)
+	}
+
+	os.Exit(1)
+}
+
 func main() {
 	var channel, filename string
-	var asCode bool
+	var asCode, quiet, silent bool
 	flag.StringVar(&channel, "channel", "", "Required: Which channel to send messages to")
 	flag.StringVar(&filename, "params-file", "", "Optional: Override slack.PostMessageParameters with JSON file")
 	flag.BoolVar(&asCode, "as-code", true, "Optional: Send message fenced by three backticks")
+	flag.BoolVar(&quiet, "quiet", true, "Optional: Suppress error message")
+	flag.BoolVar(&silent, "silent", false, "Optional: Suppress any output")
 	flag.Parse()
 
+	if silent {
+		quiet = true
+	}
+
 	if channel == "" {
-		log.Fatal("Please set an argument: -channel")
+		handleError(errors.New("please set an argument: -channel"), quiet)
 	}
 
 	token := os.Getenv("SLACK_API_TOKEN")
 	if token == "" {
-		log.Fatal("Please set an environment variable: SLACK_API_TOKEN")
+		handleError(errors.New("please set an environment variable: SLACK_API_TOKEN"), quiet)
 	}
 
 	api := slack.New(token)
 
 	body, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		log.Fatal(err)
+		handleError(err, quiet)
 	}
-	text := strings.TrimSpace(string(body))
+
+	text := string(body)
+	if !silent {
+		fmt.Print(text)
+	}
+
+	text = strings.TrimSpace(text)
+	if text == "" {
+		os.Exit(0)
+	}
+
 	if asCode {
 		text = fmt.Sprintf("```\n%s\n```", text)
 	}
 
 	params := createParams(filename)
 
-	api.PostMessage(channel, text, params)
+	_, _, err = api.PostMessage(channel, text, params)
+	if err != nil {
+		handleError(err, quiet)
+	}
 }
